721394c09654c82127d7340000f921529c51c6bc
Amanda Bienz <bienz@amandasworkmbp.unm.edu>
2022-07-21 11:43:48 -0600

    Initial commit

---
+++ tests/tutorial_test.cpp
@@ -0,0 +1,25 @@
+// EXPECT_EQ and ASSERT_EQ are macros
+// EXPECT_EQ test execution and continues even if there is a failure
+// ASSERT_EQ test execution and aborts if there is a failure
+// The ASSERT_* variants abort the program execution if an assertion fails 
+// while EXPECT_* variants continue with the run.
+
+#include "gtest/gtest.h"
+#include "tutorial_src/tutorial.h"
+
+int main(int argc, char** argv)
+{
+    MPI_Init(&argc, &argv);
+
+    ::testing::InitGoogleTest(&argc, argv);
+
+    int temp = RUN_ALL_TESTS();
+    MPI_Finalize();
+
+    return temp;
+}
+
+TEST(TLBTest, TestsIntests)
+{
+    ASSERT_EQ(0, tutorial());
+}
---
+++ tutorial.h
@@ -0,0 +1,15 @@
+#include <mpi.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+int tutorial(); 
+
+#ifdef __cplusplus
+}
+#endif
