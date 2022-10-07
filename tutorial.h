c90a08eb2935ff683d840772feac3949a8d45cfc
Amanda Bienz <abienz@Amandas-MBP.local>
2022-10-06 20:52:34 -0600

    Added initial files

--- tutorial.h
+++ tutorial.h
@@ -9,11 +9,9 @@ extern "C"
 {
 #endif
 
-double power_method_seq(int n, double* A, double* x, double* tmp, int n_iter);
-
-double power_method(int global_n, int local_n, int first_n, double* A, double* x, double* tmp, int n_iter);
-
-void simple_gather(const double* send_buffer, double* recv_buffer, int count, int root);
+void transpose(int n, double* A, double* AT);
+void transpose_datatype(int n, double* A, double* AT);
+void transpose_alltoall(int n, double* A, double* AT);
 
 #ifdef __cplusplus
 }
